7af2eeae28c13ca0dfbc27d6cf495e96d0b4910e
steven <libo@rongma.com>
2024-04-17 11:11:58 +0800

    axios

---
+++ src/api/api.js
@@ -0,0 +1,78 @@
+/*
+ * @Author: steven libo@rongma.com
+ * @Date: 2024-04-17 10:57:02
+ * @LastEditors: steven libo@rongma.com
+ * @LastEditTime: 2024-04-17 10:57:35
+ * @FilePath: \react-ts-antd\src\api\api.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import axios from 'axios';
+import { message } from 'antd';
+
+const instance = axios.create({
+  baseURL: 'http://api.example.com', // 根据实际情况设置基础 URL
+  timeout: 10000, // 请求超时时间（毫秒）
+});
+
+// 请求拦截器
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+instance.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        message.error('登录过期，请重新登录');
+      } else {
+        message.error('网络错误，请稍后再试');
+      }
+    } else if (error.request) {
+      message.error('网络错误，请检查您的网络连接');
+    } else {
+      message.error('请求失败，请稍后再试');
+    }
+    return Promise.reject(error);
+  }
+);
+
+// 封装 get 请求
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.get(url, { params })
+      .then(response => {
+        resolve(response);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+// 封装 post 请求
+export const post = (url, data = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.post(url, data)
+      .then(response => {
+        resolve(response);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+export default instance;
