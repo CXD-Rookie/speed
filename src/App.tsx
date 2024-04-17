2cbec40a2d2fe43c96e7e3e811fc355329bc9500
zhangda <kdnb150658>
2024-04-17 15:07:51 +0800

    fix: 修改布局样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 import { useNavigate, useRoutes } from "react-router-dom";
-import { NotificationOutlined } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
+import { NotificationOutlined, SlackOutlined } from "@ant-design/icons";
+import { Layout, theme, Tabs, Input } from "antd";
 
 import type { MenuProps } from "antd";
 
 import routes from "./routes/index";
 import Login from "./containers/Login/index";
 
-const { Header, Content, Sider } = Layout;
+import "@/assets/css/App.scss";
 
-const titleMenu: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
-  key,
-  label: `标题 ${key}`,
-}));
+const { Header, Content } = Layout;
+
+// const items: TabsProps["items"] = [
+//   {
+//     key: "1",
+//     label: "首页",
+//     children: "",
+//   },
+//   {
+//     key: "2",
+//     label: "游戏库",
+//     children: "",
+//   },
+// ];
 
 const siderMenu: MenuProps["items"] = [
   {
@@ -21,21 +31,6 @@ const siderMenu: MenuProps["items"] = [
     icon: <NotificationOutlined />,
     label: "gameList",
   },
-  // {
-  //   key: "info",
-  //   icon: <LaptopOutlined />,
-  //   label: "信息管理",
-  //   children: [
-  //     {
-  //       key: "info-detail",
-  //       label: "信息详情",
-  //     },
-  //     {
-  //       key: "info-look",
-  //       label: "信息查询",
-  //     },
-  //   ],
-  // },
 ];
 
 const App: React.FC = () => {
@@ -49,55 +44,53 @@ const App: React.FC = () => {
   const [breadcrumbName, setBreadcrumbName] = useState("home"); // 面包屑名称
 
   // 点击菜单
-  const handleSiderClick: MenuProps["onClick"] = ({ key, keyPath }) => {
-    const name = keyPath.reverse().join("/") || "";
-
-    setBreadcrumbName(name);
-    // if (key !== "home" && key !== "about") return;
-    // 路由跳转
-    navigate(key, {
+  const handleChangeTabs = () => {
+    navigate("gamelist", {
       replace: false,
       state: {
-        id: key,
+        id: "gamelist",
       },
     });
+    // 路由跳转
+    // navigate(key, {
+    //   replace: false,
+    //   state: {
+    //     id: key,
+    //   },
+    // });
   };
 
   return (
-    <Layout>
+    <Layout className="app-module">
       <Header className="header">
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["1"]}
-          items={titleMenu}
-        />
+        <SlackOutlined className="header-icon" />
+        <div className="header-functional-areas">
+          {/* <Tabs
+            defaultActiveKey="1"
+            items={items}
+            onChange={handleChangeTabs}
+          /> */}
+          <div onClick={handleChangeTabs}>游戏库</div>
+          <Input
+            className="search-input"
+            size="large"
+            placeholder="搜索游戏"
+            // prefix={<UserOutlined />}
+          />
+          <div>设置</div>
+        </div>
       </Header>
       <Layout>
-        <Sider width={200} style={{ background: colorBgContainer }}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-            items={siderMenu}
-            onClick={handleSiderClick}
-          />
-        </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <div style={{ margin: "16px 0" }}>{breadcrumbName}</div>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: colorBgContainer,
-            }}
-          >
-            {routeView}
-          </Content>
-        </Layout>
+        <Content
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          {routeView}
+        </Content>
       </Layout>
     </Layout>
   );
