1635506485863a579e5cf1d690758e554bb32528
steven <libo@rongma.com>
2024-05-21 21:44:58 +0800

    fix 登錄路由攔截

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 /*
- * @Author: zhangda
- * @Date: 2024-04-16 14:11:44
+ * @Author: steven libo@rongma.com
+ * @Date: 2024-05-21 20:47:13
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-05-21 20:59:42
- * @important: 重要提醒
- * @Description: 备注内容
+ * @LastEditTime: 2024-05-21 21:30:23
  * @FilePath: \speed\src\routes\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import React from "react";
 import { Navigate } from "react-router-dom";
 
 import GameList from "@/pages/GameList";
@@ -14,27 +14,48 @@ import Home from "@/pages/Home";
 import GameDetail from "@/pages/Home/GameDetail";
 import MyGames from "@/pages/Home/MyGames";
 import Login from "@/containers/Login";
+import PrivateRoute from "./PrivateRoute"; 
+
 export default [
   {
     path: "/gameList",
-    element: <GameList />,
+    element: (
+      <PrivateRoute>
+        <GameList />
+      </PrivateRoute>
+    ),
   },
   {
     path: "/home",
-    element: <Home />,
+    element: (
+      <PrivateRoute>
+        <Home />
+      </PrivateRoute>
+    ),
   },
   {
     path: "/gameDetail",
-    element: <GameDetail />,
+    element: (
+      <PrivateRoute>
+        <GameDetail />
+      </PrivateRoute>
+    ),
   },
   {
     path: "/myGames",
-    element: <MyGames />,
+    element: (
+      <PrivateRoute>
+        <MyGames />
+      </PrivateRoute>
+    ),
   },
   {
-    // 路由重定向
+    path: "/login",
+    element: <Login />,
+  },
+  {
     path: "/",
-    element: <Navigate to="/Login" />,
+    element: <Navigate to="/home" />,
   },
 ] as {
   path: string;
