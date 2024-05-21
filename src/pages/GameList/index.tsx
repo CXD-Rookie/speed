b40b2dd59bdf60c0eec67b50590191f73da7a48e
zhangda <kdnb150658>
2024-05-21 22:01:29 +0800

    fix: 修改游戏库样式

--- src/pages/GameList/index.tsx
+++ src/pages/GameList/index.tsx
@@ -2,12 +2,11 @@
  * @Author: steven libo@rongma.com
  * @Date: 2023-09-15 13:48:17
  * @LastEditors: zhangda
- * @LastEditTime: 2024-04-22 17:10:50
+ * @LastEditTime: 2024-05-21 21:56:30
  * @FilePath: \speed\src\pages\GameList\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useState, useRef, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
 
 import "./style.scss";
 
