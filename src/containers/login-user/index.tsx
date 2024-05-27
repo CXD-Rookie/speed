898493b73a01dc0ad731e752c0b16c5588a54967
zhangda <kdnb150658>
2024-05-27 18:16:15 +0800

    fix: 修改用户信息

--- src/containers/login-user/index.tsx
+++ src/containers/login-user/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Avatar, Popover } from "antd";
 import "./index.scss";
 
