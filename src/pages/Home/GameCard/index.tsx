1031f2fef1c0aba3c95246a159da2a151c971a2d
steven <libo@rongma.com>
2024-05-27 14:55:55 +0800

    fix bug

--- src/pages/Home/GameCard/index.tsx
+++ src/pages/Home/GameCard/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: zhangda
  * @Date: 2024-04-22 14:17:10
- * @LastEditors: zhangda
- * @LastEditTime: 2024-05-27 11:18:10
+ * @LastEditors: steven libo@rongma.com
+ * @LastEditTime: 2024-05-27 11:43:05
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: \speed\src\pages\Home\GameCard\index.tsx
