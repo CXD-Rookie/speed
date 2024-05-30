37eda197dace736b9f7c6e86b9d5ec11bb7b8cc1
zhangda <kdnb150658>
2024-05-30 16:59:40 +0800

    fix: 修改启动方式样式，逻辑

--- src/containers/activation-mode/index.tsx
+++ src/containers/activation-mode/index.tsx
@@ -1,16 +1,14 @@
 /*
  * @Author: zhangda
  * @Date: 2024-05-24 11:57:30
- * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-05-27 18:27:18
+ * @LastEditors: zhangda
+ * @LastEditTime: 2024-05-30 16:40:32
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: \speed\src\containers\activation-mode\index.tsx
  */
 import React, { useState } from "react";
-import { Modal, Button, Select, Input, Upload, message } from "antd";
-import { UploadOutlined } from '@ant-design/icons';
-import { RcFile, UploadChangeParam, UploadFile } from 'antd/lib/upload/interface';
+import { Modal, Button, Select } from "antd";
 
 import "./index.scss";
 
@@ -54,22 +52,19 @@ interface Game {
   start_path?: string;
 }
 
-const ActivationModal: React.FC<ActivationModalProps> = ({ isOpen = true, gameId, onClose }) => {
-  const [terrace, seTerrace] = useState([
-    {
-      label: "Steam",
-      value: "Steam",
-    },
-  ]); // 平台列表
-  const [trails, setTerrace] = useState("Select://asdfgdsa/12edsasdfc"); // 启动路径
-  const [filePath, setFilePath] = useState('');
+const ActivationModal: React.FC<ActivationModalProps> = ({
+  isOpen = true,
+  gameId,
+  onClose,
+}) => {
+  const [filePath, setFilePath] = useState(""); // 启动路径
   const [game, setGame] = useState<Game | null>(null);
 
   const handleInputChange = () => {
-    console.log("打开路径=====================",gameId)
-   
+    console.log("打开路径=====================", gameId);
+
     const requestData = JSON.stringify({
-      method: "NativeApi_SelectFilePath"
+      method: "NativeApi_SelectFilePath",
     });
     (window as any).cefQuery({
       request: requestData,
@@ -81,28 +76,31 @@ const ActivationModal: React.FC<ActivationModalProps> = ({ isOpen = true, gameId
         console.error("Query failed:", errorMessage);
       },
     });
-  
   };
 
   const handleSave = () => {
     console.log("点击报存");
-     // 这个逻辑需要在打开文件之后回调处理，这是保存的逻辑
-     const gamesListString = localStorage.getItem("speed-1.0.0.1-games");
-     if (gamesListString) {
-       try {
-         const gamesList: Game[] = JSON.parse(gamesListString);
-         const gameDetail = gamesList.find((game) => game.id === gameId); // 使用传递进来的 gameId
-         if (gameDetail) {
-           gameDetail.start_path = filePath; // 添加 start_path 属性
-           console.log(11111111111111111);
-           localStorage.setItem("speed-1.0.0.1-games", JSON.stringify(gamesList));
-           setGame(gameDetail); // 更新本地状态
-           onClose(); // 关闭弹窗
-         }
-       } catch (error) {
-         console.error('Failed to parse gamesList from localStorage', error);
-       }
-     }
+    // 这个逻辑需要在打开文件之后回调处理，这是保存的逻辑
+    const gamesListString = localStorage.getItem("speed-1.0.0.1-games");
+    if (gamesListString) {
+      try {
+        const gamesList: Game[] = JSON.parse(gamesListString);
+        const gameDetail = gamesList.find((game) => game.id === gameId); // 使用传递进来的 gameId
+
+        if (gameDetail) {
+          gameDetail.start_path = filePath; // 添加 start_path 属性
+          console.log(11111111111111111);
+          localStorage.setItem(
+            "speed-1.0.0.1-games",
+            JSON.stringify(gamesList)
+          );
+          setGame(gameDetail); // 更新本地状态
+          onClose(); // 关闭弹窗
+        }
+      } catch (error) {
+        console.error("Failed to parse gamesList from localStorage", error);
+      }
+    }
   };
 
   return (
@@ -111,32 +109,24 @@ const ActivationModal: React.FC<ActivationModalProps> = ({ isOpen = true, gameId
       open={isOpen} //isOpen
       onCancel={onClose}
       title="启动方式"
-      width={400}
+      width={"40vw"}
       centered
       footer={null}
     >
       <div className="activation-modal-content">
         <div className="content-title">启动平台：</div>
-        <Select
-          className="content-select"
-          // defaultValue={selectedNode}
-          // onChange={(value) => setSelectedNode(value)}
-        >
+        <Select className="content-select" defaultValue={"Steam"}>
           <Option value="Steam">Steam</Option>
         </Select>
-        <div className="content-title">启动路径：</div>
-        <Button onClick={(handleInputChange)}>浏览</Button>
-        {filePath && <p>File selected: {filePath}</p>}
-        {/* <Input
-          className="content-input"
-          value={trails}
-          onChange={handleInputChange}
-        />
-         <input type="file" id="fileInput" onChange={handleFileChange} />
-      {filePath && <p>File selected: {filePath}</p>} */}
-        <Button className="content-btn" type="default" onClick={handleSave}>
-          保存
-        </Button>
+        <div className="content-title">启动路径：{filePath}</div>
+        <div className="btn-box">
+          <Button className="content-btn" onClick={handleInputChange}>
+            浏览
+          </Button>
+          <Button className="content-btn" type="default" onClick={handleSave}>
+            保存
+          </Button>
+        </div>
       </div>
     </Modal>
   );
