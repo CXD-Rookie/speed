be525fff209f90d6a4393f387b494e65918dad7e
steven <libo@rongma.com>
2024-06-03 14:21:57 +0800

    fix bug

--- src/containers/login-user/index.tsx
+++ src/containers/login-user/index.tsx
@@ -2,7 +2,7 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-05-23 16:01:09
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-05-31 18:15:06
+ * @LastEditTime: 2024-06-03 11:52:05
  * @FilePath: \speed\src\containers\login-user\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,7 @@ import React, { useState, useEffect } from "react";
 import { Button, Avatar, Popover } from "antd";
 
 import SettingsModal from "../setting";
+import PayModal from "../Pay";
 import "./index.scss";
 
 interface CustomDropdownProps {}
@@ -17,6 +18,7 @@ interface CustomDropdownProps {}
 const CustomDropdown: React.FC<CustomDropdownProps> = (props) => {
   const [userInfo, setUserInfo] = useState<any>({});
   const [editOpen, setEditOpen] = useState(false);
+  const [isModalOpenVip, setIsModalOpenVip] = useState(false);
   const formatDate = (timestamp: number) => {
     const date = new Date(timestamp * 1000);
     const year = date.getFullYear();
@@ -25,12 +27,29 @@ const CustomDropdown: React.FC<CustomDropdownProps> = (props) => {
     return `${year}-${month}-${day}`;
   };
   useEffect(() => {
-    let user_info = localStorage.getItem("userInfo");
-    user_info = user_info ? JSON.parse(user_info) : {};
-    setUserInfo(user_info);
+    const fetchUserInfo = () => {
+      let user_info = localStorage.getItem("userInfo");
+      user_info = user_info ? JSON.parse(user_info) : {};
+      setUserInfo(user_info);
+    };
+
+    fetchUserInfo();
+
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === "userInfo") {
+        fetchUserInfo();
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
   }, []);
   const popoverContent = (isVip: boolean) => (
-    <div className="dropdown-content">
+    <div>
+      <div className="dropdown-content">
       <div className="avatar-box">
         <div>
           <Avatar
@@ -44,12 +63,24 @@ const CustomDropdown: React.FC<CustomDropdownProps> = (props) => {
         </div>
         <span className={isVip ? "vips" : "novips"} onClick={() => setEditOpen(true)}>编辑</span>
       </div>
-      <Button className={isVip ? "vip" : "novip"} type={"primary"}>{isVip ? "续费" : "会员充值"}</Button>
+      <Button onClick={ () => setIsModalOpenVip(true)} className={isVip ? "vip" : "novip"} type={"primary"}>{isVip ? "续费" : "会员充值"}</Button>
      
       {isVip
           ? <p>会员到期：{formatDate(userInfo.vip_expiration_time)}</p>
           : <p>解锁全新游戏体验，畅玩游戏从未有过的速度！</p>}
     </div>
+        {!!isModalOpenVip && (
+        <PayModal
+          isModalOpen={isModalOpenVip}
+          setIsModalOpen={(e) => {
+            console.log(e);
+            setIsModalOpenVip(e);
+          }}
+        />
+      )}
+    </div>
+
+    
   );
 
   return (
