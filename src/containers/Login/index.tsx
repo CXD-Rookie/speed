a0e8ebac0cb60b9e55164f8d6e488cd162eda9dd
steven <libo@rongma.com>
2024-06-05 14:10:47 +0800

    登录和关闭登录的时候 增加跳转当天页面的逻辑

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -2,7 +2,7 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-04-16 19:26:21
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-05-30 19:18:23
+ * @LastEditTime: 2024-06-05 13:48:43
  * @FilePath: \speed\src\containers\Login\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -108,11 +108,11 @@ const Login: React.FC<LoginProps> = (_props) => {
         dispatch(setIsLogin(false))
 
         // navigate("/home")
+         // eslint-disable-next-line no-restricted-globals
+        // @ts-ignore
+        window.location.reload();
+        console.log("关闭跳转");
 
-        //   CefWebInstance.call('jsCallPushClientInfo', res.data, (_AError: any, AResult: any) => {
-        //     console.log('return jsCallStartSpeed');
-        //     console.log(AResult);
-        // })
       } else {
         console.log(res, "111111111");
         setVeryCode(false);
@@ -125,6 +125,11 @@ const Login: React.FC<LoginProps> = (_props) => {
 
   const close = async () => {
     dispatch(setIsLogin(false));
+    // eslint-disable-next-line no-restricted-globals
+    // @ts-ignore
+    window.location.reload();
+    console.log("关闭跳转");
+
   };
 
   return (
