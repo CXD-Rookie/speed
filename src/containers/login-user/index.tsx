850e0a8db274a9a4e549c2dc57b2c064cb1c336d
zhangda <kdnb150658>
2024-06-06 14:36:56 +0800

    fix: 修改头像样式

--- src/containers/login-user/index.tsx
+++ src/containers/login-user/index.tsx
@@ -2,7 +2,7 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-05-23 16:01:09
  * @LastEditors: zhangda
- * @LastEditTime: 2024-06-05 17:51:44
+ * @LastEditTime: 2024-06-06 14:28:25
  * @FilePath: \speed\src\containers\login-user\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -41,10 +41,10 @@ const CustomDropdown: React.FC<CustomDropdownProps> = (props) => {
         <div className="avatar-box">
           <div>
             <UserAvatarCom
-              isVip={accountInfo?.userInfor?.is_vip}
+              isVip={accountInfo?.userInfo?.is_vip}
               isLogin={accountInfo?.isLogin}
             />
-            <span>{accountInfo?.userInfo?.nickname}</span>
+            <span className="name-text">{accountInfo?.userInfo?.nickname}</span>
           </div>
           <span
             className={isVip ? "vips" : "novips"}
