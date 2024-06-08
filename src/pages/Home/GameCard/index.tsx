b86b8b37a24e8273c7fb0fe4ef49bfb96206421c
zhangda <kdnb150658>
2024-06-08 19:23:02 +0800

    fix: 修改加速逻辑

--- src/pages/Home/GameCard/index.tsx
+++ src/pages/Home/GameCard/index.tsx
@@ -11,11 +11,15 @@ import RealNameModal from "@/containers/real-name";
 import MinorModal from "@/containers/minor";
 import PayModal from "@/containers/Pay";
 import BreakConfirmModal from "@/containers/break-confirm";
+import StopConfirmModal from "@/containers/stop-confirm";
 
 import addIcon from "@/assets/images/common/add.svg";
 import closeIcon from "@/assets/images/common/close.svg";
 import arrowIcon from "@/assets/images/common/accel-arrow.svg";
 import accelerateIcon from "@/assets/images/common/accelerate.svg";
+import rightArrow from "@/assets/images/common/right-arrow.svg";
+import acceleratedIcon from "@/assets/images/common/accelerated.svg";
+import cessationIcon from "@/assets/images/common/cessation.svg";
 
 interface GameCardProps {
   options: any;
@@ -35,6 +39,7 @@ const GameCard: React.FC<GameCardProps> = (props) => {
 
   const accountInfo: any = useSelector((state: any) => state.accountInfo); // 获取 redux 中的用户信息
   const isRealOpen = useSelector((state: any) => state.auth.isRealOpen); // 实名认证
+  const accDelay = useSelector((state: any) => state.auth.delay); // 延迟毫秒数
 
   const { getGameList, identifyAccelerationData, removeGameList } =
     useGamesInitialize();
@@ -46,20 +51,40 @@ const GameCard: React.FC<GameCardProps> = (props) => {
   const [isModalOpenVip, setIsModalOpenVip] = useState(false); // 是否是vip
 
   const [accelOpen, setAccelOpen] = useState(false); // 是否确认加速
+  const [stopModalOpen, setStopModalOpen] = useState(false); // 停止加速确认
   const [isStartAnimate, setIsStartAnimate] = useState(false); // 是否开始加速动画
-  const [selectAccelerateOption, setSelectAccelerateOption] =
-    useState<object>(); // 选择加速的数据
+  const [selectAccelerateOption, setSelectAccelerateOption] = useState<any>(); // 选择加速的数据
 
   const [isAllowAcceleration, setIsAllowAcceleration] = useState<boolean>(true); // 是否允许加速
+  const [isAllowShowAccelerating, setIsAllowShowAccelerating] =
+    useState<boolean>(true); // 是否允许显示加速中
 
   const isHomeNullCard =
     locationType === "home" && options?.length < 4 && options?.length > 0; // 判断是否是首页无数据卡片条件
 
+  // 停止加速
+  const stop = () => {
+    setStopModalOpen(false);
+    // handleStopClick(gameData);
+  };
+
+  // 加速实际操作
+  const accelerateProcessing = (option = selectAccelerateOption) => {
+    setIsAllowAcceleration(false); // 禁用立即加速
+    setIsAllowShowAccelerating(false); // 禁用显示加速中
+    setIsStartAnimate(true); // 开始加速动画
+
+    setTimeout(() => {
+      setIsAllowAcceleration(true); // 启用立即加速
+      setIsAllowShowAccelerating(true); // 启用显示加速中
+      setIsStartAnimate(false); // 结束加速动画
+    }, 5000);
+  };
+
   // 确认开始加速
   const confirmStartAcceleration = () => {
     setAccelOpen(false); // 关闭确认框
-    setIsAllowAcceleration(false); // 禁用立即加速
-    setIsStartAnimate(true); // 开始加速动画
+    accelerateProcessing();
   };
 
   // 清除选择卡片
@@ -147,6 +172,52 @@ const GameCard: React.FC<GameCardProps> = (props) => {
                 </div>
               </div>
             ) : null}
+            {/* 加速动画卡片 */}
+            {isStartAnimate && selectAccelerateOption?.id === option?.id ? (
+              <div className={"accelerate-animate-card"}>
+                <div className={"animate-text"}>加速中...</div>
+                <div
+                  className={`accelerate-animate ${
+                    isStartAnimate && "accelerate-animate-start"
+                  }`}
+                />
+              </div>
+            ) : null}
+            {/* 加速中卡片 */}
+            {isAllowShowAccelerating && option?.is_accelerate ? (
+              <div className="accelerating-card">
+                <img
+                  className="accelerating-content-img"
+                  src={acceleratedIcon}
+                  alt=""
+                />
+                <div className="accelerating-content">
+                  {locationType === "home" && (
+                    <>
+                      <div className="instant-delay">即时延迟</div>
+                      <div className="speed">
+                        {accDelay || 2}
+                        <span>ms</span>
+                      </div>
+                    </>
+                  )}
+                  <div
+                    className="enter-details"
+                    onClick={() => navigate("/gameDetails")}
+                  >
+                    <span>进入详情</span>
+                    <img src={rightArrow} alt="" />
+                  </div>
+                  <div
+                    className="down-accelerate"
+                    onClick={() => setStopModalOpen(true)}
+                  >
+                    <img src={cessationIcon} width={18} height={18} alt="" />
+                    <span>停止加速</span>
+                  </div>
+                </div>
+              </div>
+            ) : null}
             <div className="game-card-text">{option?.name}</div>
           </div>
         );
@@ -186,6 +257,14 @@ const GameCard: React.FC<GameCardProps> = (props) => {
         setAccelOpen={setAccelOpen}
         onConfirm={confirmStartAcceleration}
       />
+      {/* 停止加速确认弹窗 */}
+      {stopModalOpen ? (
+        <StopConfirmModal
+          accelOpen={stopModalOpen}
+          setAccelOpen={setStopModalOpen}
+          onConfirm={stop}
+        />
+      ) : null}
     </div>
   );
 };
