a0b027e046434f14d9c873f54447860a041d40e4
zhangda <kdnb150658>
2024-06-11 16:06:20 +0800

    fix: 修改验证逻辑

--- src/containers/login-user/index.tsx
+++ src/containers/login-user/index.tsx
@@ -2,7 +2,7 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-05-23 16:01:09
  * @LastEditors: zhangda
- * @LastEditTime: 2024-06-07 14:55:16
+ * @LastEditTime: 2024-06-11 16:05:11
  * @FilePath: \speed\src\containers\login-user\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -82,7 +82,7 @@ const CustomDropdown: React.FC<CustomDropdownProps> = (props) => {
         <Button
           onClick={() => {
             const isRealNamel = localStorage.getItem("isRealName");
-
+            handleUserInfo();
             hide();
 
             if (isRealNamel === "1") {
