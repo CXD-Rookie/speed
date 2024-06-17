b219bcb579dee03a883d6d7c9a2c1a36a17c4044
zhangda <kdnb150658>
2024-06-17 17:10:45 +0800

    fix: 修改登录验证码appid

--- src/containers/Login/tencent-captcha/index.tsx
+++ src/containers/Login/tencent-captcha/index.tsx
@@ -41,7 +41,7 @@ const TencentCatcha: React.FC<CaptchaProps> = (props) => {
 
   // 定义验证码js加载错误处理函数
   const loadErrorCallback = () => {
-    let appid = "195013408"; // 生成容灾票据或自行做其它处理
+    let appid = "191215490"; // 生成容灾票据或自行做其它处理
     let ticket =
       "terror_1001_" + appid + Math.floor(new Date().getTime() / 1000);
 
@@ -57,7 +57,7 @@ const TencentCatcha: React.FC<CaptchaProps> = (props) => {
   const handleVerifyCode = () => {
     try {
       let captcha = new (window as any).TencentCaptcha(
-        "195013408",
+        "191215490",
         codeCallback,
         {
           userLanguage: "zh",
