f402e2365a101d11483c5270902990b93aeec47f
steven <libo@rongma.com>
2024-06-24 18:15:23 +0800

    fix

--- src/common/webSocketService.ts
+++ src/common/webSocketService.ts
@@ -2,7 +2,7 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-06-21 14:52:37
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-06-24 18:04:18
+ * @LastEditTime: 2024-06-24 18:13:15
  * @FilePath: \speed\src\common\webSocketService.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -51,7 +51,7 @@ class WebSocketService {
     this.ws.onclose = () => {
       console.log('WebSocket connection closed');
       this.stopHeartbeat();
-      this.reconnect(url, onMessage, dispatch);
+      // this.reconnect(url, onMessage, dispatch);
     };
 
     this.ws.onerror = (error) => {
