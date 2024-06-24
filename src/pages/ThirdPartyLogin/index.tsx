d09fc806c9b3be7a7454bba7a6352e16b85873bd
zhangda <kdnb150658>
2024-06-24 15:50:26 +0800

    fix: 修改登录页

---
+++ src/pages/ThirdPartyLogin/index.tsx
@@ -0,0 +1,28 @@
+/*
+ * @Author: zhangda
+ * @Date: 2024-06-24 15:45:14
+ * @LastEditors: zhangda
+ * @LastEditTime: 2024-06-24 15:50:08
+ * @important: 重要提醒
+ * @Description: 备注内容
+ * @FilePath: \speed\src\pages\ThirdPartyLogin\index.tsx
+ */
+import React, { useEffect } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+
+import "./index.scss";
+
+interface ThirdPartyLoginProps {}
+
+const ThirdPartyLogin: React.FC<ThirdPartyLoginProps> = () => {
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    console.log(location, searchParams.get("code"), "进入第三方登录页");
+  }, []);
+
+  return <div className="visitor-login">登录成功</div>;
+};
+
+export default ThirdPartyLogin;
