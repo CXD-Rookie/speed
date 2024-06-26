60dbb7570fdaa2a2862750d2eb74041b07161593
steven <libo@rongma.com>
2024-06-26 20:18:19 +0800

    断网提示弹窗

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 /*
  * @Author: steven libo@rongma.com
  * @Date: 2024-04-17 10:57:02
- * @LastEditors: zhangda
- * @LastEditTime: 2024-06-25 10:26:20
+ * @LastEditors: steven libo@rongma.com
+ * @LastEditTime: 2024-06-26 20:11:19
  * @FilePath: \speed\src\api\api.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import axios from 'axios';
 import { store } from '@/redux/store';
-import { message } from 'antd';
+import { message,Modal } from 'antd';
 import { setAccountInfo } from '@/redux/actions/account-info';
 import eventBus from './eventBus';
 const instance = axios.create({
@@ -99,6 +99,13 @@ instance.interceptors.response.use(
       }
     } else if (error.request) {
       console.log('网络错误，请检查您的网络连接');
+      Modal.success({
+        content: '网络错误，请检查您的网络连接',
+        className: "popup-success-fix",
+        onOk: () => {
+          console.log("Modal closed");
+        }, // 替换为你的关闭逻辑
+      });
     } else {
       console.log('请求失败，请稍后再试');
     }
