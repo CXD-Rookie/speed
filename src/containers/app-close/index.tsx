78c3effc2961d8b1c308719b565a90e0049f95be
zhangda <kdnb150658>
2024-06-28 16:55:30 +0800

    fix: 修改关闭主程序

--- src/containers/app-close/index.tsx
+++ src/containers/app-close/index.tsx
@@ -2,14 +2,15 @@
  * @Author: zhangda
  * @Date: 2024-06-28 16:06:25
  * @LastEditors: zhangda
- * @LastEditTime: 2024-06-28 16:29:21
+ * @LastEditTime: 2024-06-28 16:49:19
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: \speed\src\containers\app-close\index.tsx
  */
 import React from "react";
 import { useSelector } from "react-redux";
-import { Modal, Radio } from "antd";
+import { Button, Modal, Radio, Checkbox } from "antd";
+import type { CheckboxChangeEvent } from "antd/es/checkbox";
 
 import "./index.scss";
 
@@ -22,6 +23,10 @@ const AppCloseModal: React.FC<AppCloseModalProps> = (props) => {
   const { open, close } = props;
 
   const accountInfo: any = useSelector((state: any) => state.accountInfo);
+
+  const onChange = (e: CheckboxChangeEvent) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
 
   return (
     <Modal
@@ -46,6 +51,12 @@ const AppCloseModal: React.FC<AppCloseModalProps> = (props) => {
           <Radio value={"1"}>隐藏任务到托盘</Radio>
           <Radio value={"2"}>关闭程序</Radio>
         </Radio.Group>
+        <Button className="content-confirm" type="primary">
+          确定
+        </Button>
+        <Checkbox className="content-no-tooltip" onChange={onChange}>
+          不再提示
+        </Checkbox>
       </div>
     </Modal>
   );
