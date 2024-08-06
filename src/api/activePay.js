539c986dc3c027e59906eaaf6478eb818b346c41
steven <libo@rongma.com>
2024-08-06 10:55:24 +0800

    fix bug

--- src/api/activePay.js
+++ src/api/activePay.js
@@ -2,7 +2,7 @@
  * @Author: steven libo@rongma.com
  * @Date: 2024-07-30 16:49:34
  * @LastEditors: steven libo@rongma.com
- * @LastEditTime: 2024-07-30 18:01:23
+ * @LastEditTime: 2024-08-06 10:50:25
  * @FilePath: \speed\src\api\activePay.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -40,7 +40,7 @@ class PayApi {
   getBanner (params) {
     // const queryString = new URLSearchParams(params).toString();
     console.log(params, "params------------")
-    return get(`${this.url}/banner/list?platform=3&cid=${params}`);
+    return get(`${this.url}/banner/list?platform=3`);
   }
 }
 
