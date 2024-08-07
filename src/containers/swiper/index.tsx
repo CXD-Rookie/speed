f39ebe4e2d00d0cc5fcf0dbf0569ccecad2d7ce7
zhangda <kdnb150658>
2024-08-07 20:05:21 +0800

    fix: 【Banner图】：只有一张Banner图时无需展示左右箭头与最下方的选择条

--- src/containers/swiper/index.tsx
+++ src/containers/swiper/index.tsx
@@ -6,7 +6,7 @@
  * @FilePath: \speed\src\containers\swiper\indeX.tsX
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Carousel } from "antd";
 import eventBus from '../../api/eventBus'; 
 import { useDispatch, useSelector } from "react-redux";
@@ -47,8 +47,13 @@ const Swiper: React.FC<SwiperProps> = ({ onImageClick }) => {
 
   return (
     <div className="swiper-box">
-      <Carousel arrows infinite={false} ref={carouselRef}>
-        {images.map(({ image_url, params }, index:any) => (
+      <Carousel
+        arrows
+        infinite={false}
+        dots={images?.length > 1}
+        ref={carouselRef}
+      >
+        {images.map(({ image_url, params }, index: any) => (
           <div key={index} onClick={() => onImageClick(params)}>
             <img
               className="imgalt"
@@ -58,22 +63,26 @@ const Swiper: React.FC<SwiperProps> = ({ onImageClick }) => {
           </div>
         ))}
       </Carousel>
-      <img
-        className="left-btn"
-        src={leftIcon}
-        width={20}
-        height={32}
-        alt=""
-        onClick={() => carouselRef?.current?.prev()}
-      />
-      <img
-        className="right-btn"
-        src={rightIcon}
-        width={20}
-        height={32}
-        alt=""
-        onClick={() => carouselRef?.current?.next()}
-      />
+      {images?.length > 1 && (
+        <Fragment>
+          <img
+            className="left-btn"
+            src={leftIcon}
+            width={20}
+            height={32}
+            alt=""
+            onClick={() => carouselRef?.current?.prev()}
+          />
+          <img
+            className="right-btn"
+            src={rightIcon}
+            width={20}
+            height={32}
+            alt=""
+            onClick={() => carouselRef?.current?.next()}
+          />
+        </Fragment>
+      )}
     </div>
   );
 };
