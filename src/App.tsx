1a5269f8efd746effb2c110188d2a542e0f1a7ff
zhangda <kdnb150658>
2024-08-07 18:58:13 +0800

    fix: 【托盘】：主界面隐藏、已加速状态下退出功能不符合预期

--- src/App.tsx
+++ src/App.tsx
@@ -370,10 +370,9 @@ const App: React.FC = (props: any) => {
 
   const stopSpeed = () => {
     //全局只给客户端调用，业务不处理,是到托盘之后邮件 弹出的关闭按钮的方法
-    let close = localStorage.getItem("client_settings");
-    let action = close ? JSON.parse(close)?.close_button_action : 2;
     //0 最小化托盘 1 关闭主程序 2 或没值弹窗提示框
     if (identifyAccelerationData()?.[0]) {
+      (window as any).NativeApi_ShowWindow(); // 唤醒主程序客户端方法
       setExitOpen(true); // 弹出关闭确认框
     } else {
       (window as any).NativeApi_ExitProcess();
