cc990c7682dcbaa3055c7537ed369b2095df77a6
steven <libo@rongma.com>
2024-08-09 11:09:04 +0800

    fixbug

--- src/containers/Pay/index.tsx
+++ src/containers/Pay/index.tsx
@@ -338,15 +338,15 @@ const PayModal: React.FC<PayModalProps> = (props) => {
                   <div className="term">{payTypes[item.type]}</div>
                   <div className="price">
                     ¥<span className="price-text">{item.month_price}</span>/月
-                    {!(firstAuth.firstAuth.first_purchase && firstAuth.firstAuth.first_renewed) && (
-                      <span className="text">¥{item.scribing_month_price}</span>
-                    )}
+                    {(firstAuth.firstAuth.first_purchase || firstAuth.firstAuth.first_renewed) && (
+                        <span className="text">¥{item.scribing_month_price}</span>
+                      )}
                   </div>
                   <div className="amount">
-                    总价：¥<span>{item.price}</span>
-                    {!(firstAuth.firstAuth.first_purchase && firstAuth.firstAuth.first_renewed) && (
-                      <span className="text">原价: ¥{item.scribing_price}</span>
-                    )}
+                  总价：¥<span>{item.price}</span>
+                  {(firstAuth.firstAuth.first_purchase || firstAuth.firstAuth.first_renewed) && (
+                    <span className="text">原价: ¥{item.scribing_price}</span>
+                  )}
                   </div>
                 </div>
               ))}
