f23296c51ba2b43d1332fc3bca73774a02d2f03b
zhangda <kdnb150658>
2024-08-09 16:17:26 +0800

    fix: 修改始终弹提示

--- src/App.tsx
+++ src/App.tsx
@@ -666,25 +666,25 @@ const App: React.FC = (props: any) => {
             });
           }
 
-          let bind_type = JSON.parse(localStorage.getItem("thirdBind") || "0");
+          let bind_type = JSON.parse(localStorage.getItem("thirdBind") || "-1");
           let type_obj: any = {
             "2": "thirdBind",
             "3": "thirdUpdateBind",
           };
+          
+          if (bind_type >= 0) {
+            if (isNewUser) {
+              setThirdBindType("bind"); // 定义成功类型
+              tracking.trackLoginSuccess("0");
+              setBindOpen(true); // 触发成功弹窗
+            } else if ([2, 3].includes(Number(bind_type))) {
+              setThirdBindType(type_obj?.[String(bind_type)]); // 定义成功类型
+              tracking.trackLoginSuccess("0");
+              setBindOpen(true); // 触发成功弹窗
+            }
 
-          // if (bind_type >= 0) {
-          //   if (isNewUser) {
-          //     setThirdBindType("bind"); // 定义成功类型
-          //     tracking.trackLoginSuccess("0");
-          //     setBindOpen(true); // 触发成功弹窗
-          //   } else if ([2, 3].includes(Number(bind_type))) {
-          //     setThirdBindType(type_obj?.[String(bind_type)]); // 定义成功类型
-          //     tracking.trackLoginSuccess("0");
-          //     setBindOpen(true); // 触发成功弹窗
-          //   }
-
-          //   localStorage.removeItem("thirdBind"); // 删除第三方绑定的这个存储操作
-          // }
+            localStorage.removeItem("thirdBind"); // 删除第三方绑定的这个存储操作
+          }
         } else {
           if (!isBindPhone) {
             localStorage.removeItem("token");
