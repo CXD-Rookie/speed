25399d1bf42d2a392d418375061eb605af748f76
zhangda <kdnb150658>
2024-08-16 15:35:00 +0800

    fix: 增加10秒延时登录

--- src/pages/ThirdPartyLogin/index.tsx
+++ src/pages/ThirdPartyLogin/index.tsx
@@ -43,11 +43,13 @@ const ThirdPartyLogin: React.FC<ThirdPartyLoginProps> = () => {
 
       if (String(state)) {
         // 关闭第三方登录 并且将token 是否第三方登录 是否新用户 3个参数存储在 加速器项目中
-        (window as any).NativeApi_YouXiaAuthComplete(
-          res?.data?.token,
-          state,
-          res?.data?.is_new_user
-        );
+        setTimeout(() => {
+          (window as any).NativeApi_YouXiaAuthComplete(
+            res?.data?.token,
+            state,
+            res?.data?.is_new_user
+          );
+        }, 10000)
       }
     } catch (error) {
       console.log(error);
