825b7ddf9015b23a69d5136cf3db3b59cdf2ee26
zhangda <kdnb150658>
2024-09-10 11:30:21 +0800

    fix: 修改我的优惠券页面

--- src/containers/login-user/custom-coupon/index.tsx
+++ src/containers/login-user/custom-coupon/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { Modal, Tabs } from "antd";
+import { Button, Modal, Tabs } from "antd";
 
 import "./index.scss";
 import noIcon from "@/assets/images/common/no-data.svg";
+import loseIcon from "@/assets/images/common/yishiyong.svg";
 
 interface CouponProps {
   open: boolean;
@@ -15,6 +16,8 @@ const CustonCoupon: React.FC<CouponProps> = (props) => {
 
   const [couponMaskData, setCouponMaskData] = useState<any>([]); // 未使用数据
   const [couponLoseData, setCouponLoseData] = useState<any>([]); // 已失效数据
+
+  const [mouseoverState, setMouseoverState] = useState(false);
 
   const onClose = () => {
 
@@ -45,15 +48,76 @@ const CustonCoupon: React.FC<CouponProps> = (props) => {
             label: `未使用（${couponMaskData?.length}）`,
             children: (
               <div className="coupon-tabs-content">
-                {couponMaskData?.length > 0 ? <div></div> : null}
+                {couponMaskData?.length >= 0 ? (
+                  <div className="content-box">
+                    <div
+                      className="mask-card card"
+                      style={{
+                        borderColor: mouseoverState ? "#f86c34" : "transparent",
+                      }}
+                    >
+                      <div className="icon-box">
+                        <div className="left" />
+                        <div className="right" />
+                        {}75折
+                      </div>
+                      <div className="text-box">
+                        <div className="title">75折卡</div>
+                        <div className="time-box">
+                          2天后过期
+                          <Button
+                            type="default"
+                            onMouseOver={() => setMouseoverState(true)}
+                            onMouseLeave={() => setMouseoverState(false)}
+                          >
+                            立即使用
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="no-data-box">
+                    <img src={noIcon} alt="" />
+                    <div>暂无优惠券</div>
+                  </div>
+                )}
               </div>
             ),
           },
           {
             key: "lose",
             label: `已失效（${couponLoseData?.length}）`,
-            // disabled: loading,
-            children: <div></div>,
+            children: (
+              <div className="coupon-tabs-content">
+                {couponLoseData?.length >= 0 ? (
+                  <div className="content-box">
+                    <div
+                      className="lose-card card"
+                      style={{
+                        borderColor: mouseoverState ? "#f86c34" : "transparent",
+                      }}
+                    >
+                      <div className="icon-box">
+                        <div className="left" />
+                        <div className="right" />
+                        {}75折
+                      </div>
+                      <div className="text-box">
+                        <div className="title">75折卡</div>
+                        <div className="time-text">有效期至</div>
+                      </div>
+                      <img className="lose-icon" src={loseIcon} alt="" />
+                    </div>
+                  </div>
+                ) : (
+                  <div className="no-data-box">
+                    <img src={noIcon} alt="" />
+                    <div>暂无优惠券</div>
+                  </div>
+                )}
+              </div>
+            ),
           },
         ]}
       />
