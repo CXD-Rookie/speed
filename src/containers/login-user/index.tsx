15665aed70d73553ee58f4e1d1e5e8fa19bb6f0c
zhangda <kdnb150658>
2024-11-05 18:20:36 +0800

    fix: 点击头像下拉框中的充值入口区域，未弹出会员充值弹窗

--- src/containers/login-user/index.tsx
+++ src/containers/login-user/index.tsx
@@ -217,7 +217,7 @@ const CustomDropdown: React.FC<CustomDropdownProps> = (props) => {
             dispatch(setMinorState({ open: true, type: "recharge" })); // 关闭实名认证提示
             return;
           } else {
-            dispatch(setPayState({ open: false, couponValue: {} })); // 关闭会员充值页面
+            dispatch(setPayState({ open: true, couponValue: {} })); // 关闭会员充值页面
           }
         }}
       />
