d7ad4fc7700a49d618f1445cb3a161bc10ac5e5a
zhangda <kdnb150658>
2024-11-29 11:32:22 +0800

    fix: 【埋点】：付费页支付成功上报数据及格式异常

--- src/containers/Pay/new.tsx
+++ src/containers/Pay/new.tsx
@@ -196,11 +196,11 @@ const PayModal: React.FC = (props) => {
           }
 
           if (status === 2) {
-            const goods = res?.data?.pay_type;
+            const goods = res?.data?.type;
             const buy = firstAuth.firstAuth.first_purchase ? 1 : 2;
             const foreground = localStorage.getItem("isBuyFirstVisit");
             const firstVisit = foreground === "1" ? 0 : 1;
-
+            
             tracking.trackPurchaseSuccess(
               `goods=${goods};buy=${buy};firstVisit=${firstVisit}`
             );
