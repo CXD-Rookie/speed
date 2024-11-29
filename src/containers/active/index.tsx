f5d2579b2b0a71e572ebc53f8669d88410367a0b
zhangda <kdnb150658>
2024-11-29 14:18:19 +0800

    fix: 修改【注册】：注册成功后的领取成功弹窗中，不应该展示有效期信息

--- src/containers/active/index.tsx
+++ src/containers/active/index.tsx
@@ -113,7 +113,7 @@ const Active: React.FC = (props) => {
           </span>
         </p>
         {/* 如果领取弹窗是通过兑换码弹出的，那么在兑换类型为时间兑换码，也就是 type = 1时不展示 */}
-        {value?.type !== 1 && (
+        {value?.type && value?.type !== 1 && (
           <h6>
             {!isApproximatelyThirtyYearsApart(currencyInfo?.vip_time) &&
               "有效期至" + formatDate(currencyInfo?.vip_time || 0)}
