9a593fa1bc066038852dd571b13612e177d76cab
zhangda <kdnb150658>
2025-01-02 15:38:33 +0800

    fix: 修改api传值增加adid百度推广

--- src/api/api.js
+++ src/api/api.js
@@ -34,6 +34,7 @@ instance.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token') || '';
     const localMchannel = localStorage.getItem("mchannel");
+    const adid = localStorage.getItem("dvid"); // 推广adid
 
     const noToken = ["api/v1/game/process/blacklist"]; // 不需要传userToken的接口
     const isToken = noToken.some((item) => config?.url.includes(item)); // 不需要传userToken的接口
@@ -51,7 +52,8 @@ instance.interceptors.request.use(
     }
 
     config.headers.Mchannel = localMchannel;
-    
+    config.headers.Adid = adid; // 
+
     return config;
   },
   error => {
