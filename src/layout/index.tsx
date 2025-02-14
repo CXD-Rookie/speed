96d7dd77c10abeb97b4271322971b04c06cd4be0
zhangda <kdnb150658>
2025-02-14 16:08:31 +0800

    fix: 修改升级判断逻辑不符合预期

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -561,11 +561,17 @@ const Layouts: React.FC = () => {
             version?.web_version,
             version?.now_version
           );
+
+          // 当前普通客户端升级版本和当前前端升级版本进行比较
+          const maxLocalVersion: any = compareVersions(
+            versionNowRef.current,
+            envWebVersion
+          );
           
           // 如果客户端或者前端触发升级
           if (
-            (interim?.relation === 2 && maxVersion?.relation === 2) ||
-            (webInterim?.relation === 2 && maxVersion?.relation === 1)
+            (interim?.relation === 2 && maxLocalVersion?.relation === 1) ||
+            (webInterim?.relation === 2 && maxLocalVersion?.relation === 2)
           ) {
             // 版本比较信息锁
             const versionLock = JSON.parse(
@@ -787,7 +793,6 @@ const Layouts: React.FC = () => {
         nativeVersion(), // 读取客户端版本
         initialSetup(), // 初始设置
       ]);
-      console.log(store?.getState());
       
       if (store.getState().accountInfo?.isLogin) {
         await landFirstTrigger();
